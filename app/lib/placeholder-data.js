21d0754748f4a7a5ed20d45ceade93f48a937f49
peduke <pedro.duque@productmadness.com>
2023-12-10 23:12:29 +0100

    initial commit

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -26,7 +26,7 @@ const customers = [
     id: '3958dc9e-737f-4377-85e9-fec4b6a6442a',
     name: 'Hector Simpson',
     email: 'hector@simpson.com',
-    image_url: '/customers/hector-simpson.png',
+    image_url: '/customers/hector-simpson.png'
   },
   {
     id: '50ca3e18-62cd-11ee-8c99-0242ac120002',
